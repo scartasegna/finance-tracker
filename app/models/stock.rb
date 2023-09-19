81bce81f281de0109678e301b16b7739fc254c05
Santiago Cartasegna <santiago.cartasegna@mejuri.com>
2023-09-19 14:29:17 -0300

    added stock check funtionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ApplicationRecord
     require 'httparty'
+
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
  
     def self.new_lookup(ticker_symbol)
         ticker_response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:sandbox_api_key]}")
